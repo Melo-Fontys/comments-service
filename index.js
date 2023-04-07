5ae920b4d896569151ac641db4fccc06e77cc5ae
Alexandra Ioan <alexandra@eitr.co>
2023-04-07 13:15:14 +0200

    adding comment moderation

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const {randomBytes} = require('crypto')
 const bodyParser = require("body-parser")
-const {post} = require("axios");
-const app = express();
 const cors = require("cors");
 const axios = require("axios");
 
-app.use(cors())
+const app = express();
+
 app.use(bodyParser.json())
+app.use(cors())
 
 const commentsByPostId = {};
 
@@ -20,23 +20,48 @@ app.post('/recommendations/:id/comments', async (req, res) => {
     const {content} = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({id: commentId, content})
+    comments.push({id: commentId, content, status: "pending"})
 
-    commentsByPostId[req.params.id] = comments
+    commentsByPostId[req.params.id] = comments;
 
     await axios.post("http://localhost:8005/events", {
         type: "CommentCreated",
         data: {
-            id: commentId, content,
-            postId: req.params.id
+            id: commentId,
+            content,
+            postId: req.params.id,
+            status: "pending"
         }
     })
 
     res.status(201).send(comments)
-})
+});
 
-app.post('/events', (req, res) =>{
-    console.log("Event received", req.body.type);
+app.post('/events', async (req, res) => {
+    console.log("Event received: ", req.body.type);
+
+    const {type, data} = req.body;
+
+    if (type === "CommentModerated") {
+        const {postId, id, status, content} = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+
+        await axios.post("http://localhost:8005/events", {
+            type: "CommentUpdated",
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        })
+    }
 
     res.send({})
 });
